9bcd2c8a74b9db2400b54bc12861cee8442ab01f
jcamilito <juancamiloespinalgomez47@gmail.com>
2023-08-09 10:38:59 -0500

    sugundo commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,31 @@
-import user from "../models/user.model.js";
- 
+import User from "../models/user.model.js"; 
+import bcryptjs from 'bcryptjs';
+import { createTokenAccess } from "../libs/jwt.js";
 
-export const register = ( req, res) => {
+export const register = async ( req, res) => {
     const{email, password, username} = req.body;
 
-    console.log(email, password, username);
-    res.send("registrando")
+    // console.log(email, password, username);
+    // res.send("registrando")
+    try{
+        const passwordHash = await bcryptjs.hash(password, 10)
+        const newUser = new User({
+            username,
+            email,
+            password:passwordHash
+            
+        });
+
+        const userSaved = await newUser.save();
+        const token = await createTokenAccess({ id: userSaved._id})
+        res.cookie('token', token)
+        res.status(201).json({
+            id:userSaved._id,
+            username:userSaved.username,
+            email:userSaved.email
+        });
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
 }
-export const login = ( req, res) => res.send("login")
+export const login = ( req, res) => res.send("login") 
